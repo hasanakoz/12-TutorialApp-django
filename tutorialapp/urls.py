4e1be3d59ead4b8793b38a3dc1c66f495fab0ad9
hasan <hasanakoz1986@gmail.com>
2023-01-09 01:57:58 +0300

    url.py done, corsheaders added

---
+++ tutorialapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import TutorialView
+
+router = DefaultRouter()
+router.register('tutorials', TutorialView)
+
+urlpatterns = router.urls
